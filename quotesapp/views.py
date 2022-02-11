076181bf361f605b361540e4f3f540ee647279cd
saikiranreddyenugala <77818423+saikiranreddyenugala@users.noreply.github.com>
2022-02-10 21:49:07 -0600

    create serializer of quote category

--- quotesapp/views.py
+++ quotesapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
-
+from quotesapp.models import QuoteCategory
+from rest_framework import serializers
 # Create your views here.
+
+class QuoteCategorySerializer(serializers.Serializer):
+    category_name = serializers.CharField()
+    class Meta:
+        model = QuoteCategory
+        fields = ["id", "category_name"]
+
+
 def reverse(request):
     return redirect('')
 
-def index(request):
-    context = {}
-    return render(request, 'index.html', context)
+
