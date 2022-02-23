400ecf77fb6897335b67b98c679d23ec3eb7d49e
Kurra <S542409@nwmissouri.edu>
2022-02-23 03:10:38 -0600

    add about page url to urls.py

--- quotesapp/urls.py
+++ quotesapp/urls.py
@@ -21,6 +21,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('', include('social_django.urls')),
     path('logout', views.logout),
     path('reverse/', views.reverse, name='reverse'),
