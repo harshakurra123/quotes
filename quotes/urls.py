b7e19ec65fa6a52d1ca14c554c20069538af40e4
saikiranreddyenugala <77818423+saikiranreddyenugala@users.noreply.github.com>
2022-02-22 21:42:08 -0600

    add static root to urls

--- quotes/urls.py
+++ quotes/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from quotes import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quotesapp.urls'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
